4e79aa7a2273937f4c055185c1173691d7802ca6
Florian Klinger <github@burrotinto.de>
2021-12-08 21:58:20 +0100

    java 8

--- src/main/java/de/burrotinto/jarvis/rest/GpioBridge.java
+++ src/main/java/de/burrotinto/jarvis/rest/GpioBridge.java
@@ -4,6 +4,7 @@ import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.RaspiPin;
+import lombok.var;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
